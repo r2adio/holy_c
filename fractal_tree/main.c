2e3a75de6e3d54b48ee843925b0d7749a3292f9a
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-11 22:56:39 +0530

    feat(fractal_tree): add root branch/trunk of fractal tree

--- fractal_tree/main.c
+++ fractal_tree/main.c
@@ -1,14 +1,27 @@
 #include "raylib.h"
+#include <math.h>
 
-void fractal_tree(float x, float y, int width) { DrawPixel(x, y, RAYWHITE); }
+#define COLOR RAYWHITE
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 450
+#define SIZE_SCALE (WIN_HEIGHT * 0.1)
+
+void fractal_tree(float x, float y, int size, float width, float angle) {
+  if (size < 1)
+    return;
+  Vector2 start = {x, y};
+  Vector2 end = {x + cosf(angle) * size * SIZE_SCALE,
+                 y + sinf(angle) * size * SIZE_SCALE};
+  DrawLineEx(start, end, width, COLOR);
+}
 
 int main(void) {
-  InitWindow(800, 450, "Fractal Tree");
+  InitWindow(WIN_WIDTH, WIN_HEIGHT, "Fractal Tree");
   SetTargetFPS(3);
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    fractal_tree(100, 200, 10);
+    fractal_tree(WIN_WIDTH * 0.5, WIN_HEIGHT - 50, 5, 10, -90 * DEG2RAD);
     EndDrawing();
   }
   CloseWindow();
