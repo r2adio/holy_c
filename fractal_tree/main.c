61d8eab6f762ed529b057e7a4ed6eb9b7bca6429
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-03-12 17:47:27 +0530

    feat(fractal_tree): drawing branches recursively.

--- fractal_tree/main.c
+++ fractal_tree/main.c
@@ -2,17 +2,26 @@
 #include <math.h>
 
 #define COLOR RAYWHITE
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 450
-#define SIZE_SCALE (WIN_HEIGHT * 0.1)
+#define WIN_WIDTH 1280
+#define WIN_HEIGHT 720
+#define SIZE_SCALE (WIN_HEIGHT * 0.05)
+#define ANGLE_VARIANCE 0.8
 
-void fractal_tree(float x, float y, int size, float width, float angle) {
-  if (size < 1)
+void fractal_tree(float x, float y, float size, float width, float angle,
+                  int depth) {
+  if (depth < 0)
     return;
+
   Vector2 start = {x, y};
   Vector2 end = {x + cosf(angle) * size * SIZE_SCALE,
                  y + sinf(angle) * size * SIZE_SCALE};
   DrawLineEx(start, end, width, COLOR);
+  depth--;
+
+  fractal_tree(end.x, end.y, size - 0.70, width * 0.7, angle - ANGLE_VARIANCE,
+               depth);
+  fractal_tree(end.x, end.y, size - 0.70, width * 0.7, angle + ANGLE_VARIANCE,
+               depth);
 }
 
 int main(void) {
@@ -21,7 +30,7 @@ int main(void) {
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    fractal_tree(WIN_WIDTH * 0.5, WIN_HEIGHT - 50, 5, 10, -90 * DEG2RAD);
+    fractal_tree(WIN_WIDTH * 0.5, WIN_HEIGHT - 10, 5, 10, -90 * DEG2RAD, 40);
     EndDrawing();
   }
   CloseWindow();
