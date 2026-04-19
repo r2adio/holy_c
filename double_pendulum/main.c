1d3aa5cd242fd2c75491864eaab639cd1ac7b9c4
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-04-19 20:07:39 +0530

    feat(double_pendulum): customize the base

--- double_pendulum/main.c
+++ double_pendulum/main.c
@@ -5,10 +5,32 @@
 
 #define WIDTH 900
 #define HEIGHT 600
+
+#define L1 250
+#define L2 200
+#define BAR_HEIGHT 40.0f
+#define BAR_WIDTH 800.0f
 #define BOB_RAD 15
 
-void draw(size_t len, Vector2 start, float_t theta) {
-  DrawLineEx((Vector2){100.0f, 40.0f}, (Vector2){800.0f, 40.0f}, 5.0f, WHITE);
+void drawBase(Vector2 s) {
+  const float deg = 135.0f * DEG2RAD;
+  const float len = 40.0f;
+  const float baseStartX = 100.0f;
+  const float baseEndX = BAR_WIDTH;
+  const float spacing = 28.0f;
+
+  for (float x = baseStartX + spacing; x < baseEndX; x += spacing) {
+    Vector2 top = (Vector2){x, BAR_HEIGHT};
+    Vector2 end = (Vector2){top.x + len * sinf(deg), top.y + len * cosf(deg)};
+    DrawLineEx(end, top, 3.0f, GRAY);
+  }
+  DrawLineEx((Vector2){baseStartX, BAR_HEIGHT}, (Vector2){baseEndX, BAR_HEIGHT},
+             5.0f, WHITE);
+  // DrawRectangleLinesEx((Rectangle){1, 2, 3, 4}, 5.0f, WHITE);
+}
+
+void drawPendulum(size_t len, Vector2 start, float_t theta) {
+  drawBase(start);
   Vector2 end =
       (Vector2){start.x + len * sinf(theta), start.y + len * cosf(theta)};
   DrawLineEx(start, end, 3, WHITE);
@@ -18,10 +40,10 @@ void draw(size_t len, Vector2 start, float_t theta) {
 int main(void) {
   InitWindow(WIDTH, HEIGHT, "double pendulum");
   SetTargetFPS(60);
-  Vector2 start_pos = (Vector2){WIDTH * 0.5, 40};
+  Vector2 start_pos = (Vector2){WIDTH * 0.5, BAR_HEIGHT};
   while (!WindowShouldClose()) {
     BeginDrawing();
-    draw(200, start_pos, 0 * DEG2RAD);
+    drawPendulum(L1, start_pos, 0 * DEG2RAD);
     EndDrawing();
   }
   return EXIT_SUCCESS;
