7933d546ed6bb5ce97cb0ae07362bfdb59d429c0
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-04-20 20:35:58 +0530

    feat(double_pendulum): add II pendulum, bob linked by strings

--- double_pendulum/main.c
+++ double_pendulum/main.c
@@ -29,12 +29,19 @@ void drawBase(Vector2 s) {
   // DrawRectangleLinesEx((Rectangle){1, 2, 3, 4}, 5.0f, WHITE);
 }
 
-void drawPendulum(size_t len, Vector2 start, float_t theta) {
-  drawBase(start);
+void drawPendulum(size_t len, Vector2 start, float_t theta, float bob_rad) {
+  // drawBase(start);
   Vector2 end =
       (Vector2){start.x + len * sinf(theta), start.y + len * cosf(theta)};
   DrawLineEx(start, end, 3, WHITE);
-  DrawCircleV(end, BOB_RAD, GRAY);
+  DrawCircleV(end, bob_rad, GRAY);
+}
+void drawDPendulum(Vector2 start, float_t theta1, float_t theta2) {
+  drawBase(start);
+  Vector2 end =
+      (Vector2){start.x + L1 * sinf(theta1), start.y + L1 * cosf(theta1)};
+  drawPendulum(L2, end, theta2, L2 * 0.1);
+  drawPendulum(L1, start, theta1, L1 * 0.1);
 }
 
 int main(void) {
@@ -43,7 +50,8 @@ int main(void) {
   Vector2 start_pos = (Vector2){WIDTH * 0.5, BAR_HEIGHT};
   while (!WindowShouldClose()) {
     BeginDrawing();
-    drawPendulum(L1, start_pos, 0 * DEG2RAD);
+    // drawPendulum(L1, start_pos, 0 * DEG2RAD);
+    drawDPendulum(start_pos, 50 * DEG2RAD, -30 * DEG2RAD);
     EndDrawing();
   }
   return EXIT_SUCCESS;
