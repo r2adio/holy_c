cae788389531984b2587bd8c80ddc8075af3ae1f
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-06-17 17:48:56 +0530

    (sierpinski): initial commit

---
+++ sierpinski_triangle/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "sierpinski-triangle";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ gcc gnumake pkg-config ];
+          buildInputs = with pkgs; [ raylib ];
+        };
+      });
+    };
+}
+
---
+++ sierpinski_triangle/main.c
@@ -0,0 +1,19 @@
+#include "raylib.h"
+
+int main(void) {
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+
+  InitWindow(screenWidth, screenHeight, "Raylib + Nix Test");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Congrats! ", 140, 200, 20, LIGHTGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
