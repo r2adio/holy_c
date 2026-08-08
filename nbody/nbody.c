5d59bd0dd4dcb6c01e6f87dcce8c244869cee674
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-08-08 11:43:37 +0530

    feat(nbody): generate bodies and render those in raylib window

--- nbody/nbody.c
+++ nbody/nbody.c
@@ -1,18 +1,100 @@
 #include <raylib.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define WIN_WIDTH  900
 #define WIN_HEIGHT 600
+#define N_COLORS   7
+
+typedef struct { // Vector2 struct with double
+  double x, y;
+} Vector2D;
+
+typedef struct {
+  Vector2D pos;
+  Vector2D vel;
+  Vector2D acc;  // acceleration
+  Vector2D frce; // force
+  double radius;
+  double mass;
+  Color color;
+  uint8_t active; // 0 or 1
+} Body;
+
+typedef struct {
+  double minR;
+  double maxR;
+  double initVel;
+  double dt;
+} Configuration;
+
+double getRandomD(double min, double max) {
+  return min + ((double)rand() / RAND_MAX) * (max - min);
+}
+
+Color getBodyColor(Color colors[N_COLORS], size_t i) {
+  return colors[i];
+}
+
+/**
+ * @brief define a single body's properties
+ *
+ * @param b         holds properties of a body
+ * @param colors    random color for every body
+ * @param N_COLORS  number of colors in colors[]
+ * @param cfg       configuration of each body
+ */
+void generateBody(Body *b, Color colors[N_COLORS], Configuration *cfg) {
+  b->pos.x = getRandomD(0, WIN_WIDTH);
+  b->pos.y = getRandomD(0, WIN_HEIGHT);
+  b->vel.x = getRandomD(-cfg->initVel, cfg->initVel);
+  b->vel.y = getRandomD(-cfg->initVel, cfg->initVel);
+
+  b->acc.x = 0;
+  b->acc.y = 0;
+  b->frce.x = 0;
+  b->frce.y = 0;
+
+  b->radius = getRandomD(cfg->minR, cfg->maxR);
+  b->color = getBodyColor(colors, getRandomD(0, N_COLORS - 1)); // random color
+  b->mass = getRandomD(b->radius * b->radius, b->radius * b->radius * 10);
+
+  b->active = 1; // is active
+}
+
+// @brief generate `n` number of bodies with set color and configurations
+void setBodies(Body *b, size_t n, Color colors[N_COLORS], Configuration *cfg) {
+  for (size_t i = 0; i < n; i++) generateBody(&b[i], colors, cfg);
+}
+
+// @brief render active bodies in raylib window
+void renderBodies(Body *b, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    if (b[i].active)
+      DrawCircle((float)b[i].pos.x, (float)b[i].pos.y, (float)b[i].radius, b[i].color);
+  }
+}
 
 int main(void) {
   InitWindow(WIN_WIDTH, WIN_HEIGHT, "nbody simulation");
   SetTargetFPS(60);
 
+  srand(time(NULL));
+  Color colors[N_COLORS] = {VIOLET, WHITE, BLUE, GREEN, YELLOW, ORANGE, RED};
+
+  size_t n = 100; // number of active bodies
+  Body *bodies = malloc(n * sizeof(Body));
+  Configuration cfg = {.minR = 1, .maxR = 9, .initVel = 15};
+  setBodies(bodies, n, colors, &cfg);
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-
+    renderBodies(bodies, n); // entry point
     EndDrawing();
   }
   CloseWindow();
+  free(bodies);
   return 0;
 }
